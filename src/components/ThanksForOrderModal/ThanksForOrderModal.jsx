e8e66c2438ca36e20bf14f7549040763c230fb54
Max Maievskyi <7742694@gmail.com>
2023-05-02 18:34:07 +0200

    Added NotFoundPage, Thanks for order modal

--- src/components/ThanksForOrderModal/ThanksForOrderModal.jsx
+++ src/components/ThanksForOrderModal/ThanksForOrderModal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import s from "./style.module.css";
-import flower from "./thanks2.png";
+import flower from "./thanks.png";
 import { useDispatch } from "react-redux";
 import { deleteAllFromBasket } from "../../redux/basketSlice";
 import { Link } from "react-router-dom";
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Container from "../../components/Container/Container";
+import Button from "../../components/Button/Button";
+import img from "./404.png";
+import { Link } from "react-router-dom";
+import s from "./style.module.css";
+
 
 export default function NotFoundPage() {
   return (
     <main>
-      <Container>NotFoundPage</Container>
+      <Container>
+        <div className={s.page}>
+          <img src={img} alt="Page is not found" />
+          <p>Oops, something happened, this page is missing!</p>
+          <Link to="/greenshop">
+            <Button text="back to home page" />
+          </Link>
+        </div>
+      </Container>
     </main>
   );
 }
